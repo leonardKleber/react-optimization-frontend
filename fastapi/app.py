efeecbd8cd1001653d03a73ee376b2e4a9c2ae27
leonardKleber <leonard.kleber@outlook.com>
2023-02-22 00:47:29 +0100

    Update api for settings view

--- fastapi/app.py
+++ fastapi/app.py
@@ -2,42 +2,60 @@ import json
 from fastapi import FastAPI
 
 
+UID_LIST = ['abcd1']
+
+
 app = FastAPI()
 
 
 @app.get('/')
 def index():
-    return {'data': 'Optimization API'}
+    return {'status': 200}
 
 
-# ---------------------------------------------------------------------
+# *********************************************************************
 #
 # ALL METHODS FOR THE SETTINGS VIEW
 #
-# send_settings()
+# check_uid()
+# - Checks if there is a settings form for a given uid.
+#
+# provide_settings_form()
 # - Recieves a UID sting from the frontend.
 # - Sends a JSON file with the parameter settings form for the 
 #   optimization process to the front end.
 #
-# recieve_settings()
+# get_optimization_parameters()
 # - Recieves the users chosen parameters from the parameter settings 
 #   form from the backend.
 # - Sends a confirmation response, that data could be read.
 #  
-# ---------------------------------------------------------------------
-@app.post('/send_settings')
-def send_settings(uid: str):
-    # TODO: Check if there is a form for the UID
-    # TODO: This code assumes that the ID is always the example UID.
-    if uid == 'abcd1':
-        settings = json.load(open('./examples/example_settings.json', 'r'))
-        return settings
+# *********************************************************************
+@app.post('/check_uid')
+def check_uid(uid: str):
+    if uid in UID_LIST:
+        return {
+            'status': 200,
+            'data': True
+        }
     else:
-        return {'error': 'No preset with this UID can be found.'}
+        return {
+            'status': 200,
+            'data': False
+        }
 
 
-@app.post('/recieve_settings')
-def recieve_settings(settings: dict):
-    # TODO: Start optimization process with the given values.
-    # TODO: Return a success message.
-    return settings
+@app.post('/provide_settings_form')
+def provide_settings_form(uid: str):
+    # TODO: This code assumes that the ID is always the example UID.
+    settings = json.load(open('./examples/example_settings.json', 'r'))
+    return {
+        'status': 200,
+        'data': settings
+    }
+
+
+@app.post('/get_optimization_parameters')
+def get_optimization_parameters(values: dict):
+    # TODO: Store or process the parameters for the optimization.
+    return {'status': 200}
